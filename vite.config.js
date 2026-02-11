849e481e323601d768b5304d468263864bfd8887
NNeonael <neonael.angcaya.nazareno@gmail.com>
2026-02-11 11:43:03 +0800

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { copyFileSync } from 'fs'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'copy-sw',
-      closeBundle() {
-        copyFileSync('public/firebase-messaging-sw.js', 'dist/firebase-messaging-sw.js')
-      }
-    }
-  ],
+  plugins: [react()],
   build: {
     outDir: 'dist',
   }
