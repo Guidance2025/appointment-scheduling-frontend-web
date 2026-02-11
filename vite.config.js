dd70ce45963a83113ef5e7d9979fba642c8992d4
NNeonael <neonael.angcaya.nazareno@gmail.com>
2026-02-11 11:43:58 +0800

    fix: serve firebase-messaging-sw.js correctly (#45)
    * Login Page - Appointment Page - Calendar Page
    
    * update UI on appointment table / clean some code
    
    * fix ui / fix project structure
    
    * Fix Cleaned : .gitignore
    
    * update
    
    * cleaned
    
    * Login Page - Appointment Page - Calendar Page / Admin Page (Not Yet C…
    
    * clean
    
    * clean
    
    * added ui & consumed backednd  - profile - notitfication - appointments - nav
    
    * features added
    
    * fix commiot
    
    * fix bugs
    
    * added : new feature
    
    * fix
    
    * fix
    
    * latest update
    
    * latest update v2
    
    * feat: add self assesment
    
    * new-features-added
    
    * update fix
    
    * fix conflict
    
    * fix build fail
    
    * fix : post feature
    
    * added new mood trend feature
    
    * fix
    
    * added feature
    
    * fix yml
    
    * fix
    
    * fixing post feature
    
    * update posts
    
    * added new feature
    
    * added new feature
    
    * added feature
    
    * added new feature
    
    * added feature
    
    * Add Vercel Configuration
    
    * update
    
    * fix env config
    
    * add auto prompt for notification
    
    * fix notification
    
    * fix prod notification
    
    * fix prod notification
    
    * fix config
    
    * fix config
    
    * fix
    
    * fix notif
    
    * fix
    
    * fix
    
    * fix notif
    
    * fix fcm service
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix vercel build fail
    
    * fix notification
    
    * fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
   build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name === 'firebase-messaging-sw.js') {
-            return '[name][extname]';
-          }
-          return 'assets/[name]-[hash][extname]';
-        }
-      }
-    }
+    outDir: 'dist',
   }
 })
